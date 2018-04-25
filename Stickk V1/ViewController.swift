7a30f697a92cd31b6c10f953874fb0591c4161cc
Zeba Hashimi <zeba@zebas-air-2.t-mobile.com>
2018-04-25 14:32:12 -0700

    first commit

--- Stickk V1/ViewController.swift
+++ Stickk V1/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print (buttonCount)
+        
+        if buttonCount >= 2 {
+        
+        view.backgroundColor = UIColor.blue
+        
+        myLabel.text = "Hi, how are you doing?"
+            
+        }
+    
+        if buttonCount >= 3 {
+            
+            view.backgroundColor = UIColor.green
+            
+            myLabel.text = "Ooooh it's so cool?"
+    }
+    
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
