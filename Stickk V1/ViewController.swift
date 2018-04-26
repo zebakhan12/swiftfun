9f27d7d3681202fd60e38ea800803637df2711f2
Unknown <zebahashimi@gmail.com>
2018-04-26 15:29:38 -0700

    Now we can add and subtract!

--- Stickk V1/ViewController.swift
+++ Stickk V1/ViewController.swift
@@ -11,14 +11,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var buttonCount = 0
+// var buttonCount = 0
     
     @IBOutlet weak var myLabel: UILabel!
     
+    @IBOutlet weak var topTextField: UITextField!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        buttonCount = buttonCount + 1
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+
+        }
+            
+        
+       // print(topTextField.text!)
+        
+       // print(bottomTextField.text!)
+        
+        
+        /*
+        buttonCount += 1
         
         print (buttonCount)
         
@@ -36,7 +61,7 @@ class ViewController: UIViewController {
             
             myLabel.text = "Ooooh it's so cool?"
     }
-    
+    */
         
     }
     
